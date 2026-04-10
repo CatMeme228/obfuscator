1117011ad13d16d3df672f3c8afeb68ac6ae7cd8
Анатолий Беляев <tolabelaev1251@gmail.com>
2026-04-10 16:38:24 +0300

    сделано удаление пробелов

--- obfuscator/libs/space_remover.cpp
+++ obfuscator/libs/space_remover.cpp
@@ -2,75 +2,69 @@
 #include <ctype.h>
 #include "myHeader.h"
 
+static int is_id_char(int c) {
+    return isalnum(c) || c == '_';
+}
+
 int space_remover(FILE* input, FILE* output) {
     if (!input || !output) return -1;
 
-    int stringStatus = 0;   // 0 - код, 1 - строка, 2 - символ; строки НЕ ТРОГАЕМ!!!
-    int needSpace = 0;
-    int headFlag = 0;
+    int stringStatus = 0;   //0- код, 1- строка, 2- константа; 1-2 НЕ ТРОГАЕМ
+    int hasWhitespace = 0;  // был ли пробел
+    int lastChar = 0;       // последний не пробел
+    int headFlag = 0;       // директивы
     char curSym, next;
 
     while ((curSym = getc(input)) != EOF) {
         switch (stringStatus) {
-        case 0: 
+        case 0:
 
-            //define include
-            if (curSym == '#') headFlag = 1;
+            //директива
+            if (curSym == '#')
+                headFlag = 1;
 
             if (isspace(curSym)) {
-
+                // скип директив и сохранение перевода строк
                 if (headFlag && curSym == '\n') {
-                    headFlag=0;
                     putc(curSym, output);
-                    needSpace = 0;   // сбросить флаг пробела
+                    headFlag = 0;
+                    hasWhitespace = 0;   
                     break;
                 }
-                needSpace = 1;
+                // нужен пробел (только 1)
+                hasWhitespace = 1;
                 break;
             }
 
-            // Непробельный символ – сначала, если нужно, выводим пробел
-            if (needSpace) {
-                putc(' ', output);
-                needSpace = 0;
+            if (hasWhitespace) {
+                //между ключевыми словами
+                if (lastChar != 0 && is_id_char(lastChar) && is_id_char(curSym)) putc(' ', output);
+                hasWhitespace = 0;
             }
 
-            // смена состояния
-            if (curSym == '"') {
-                stringStatus = 1;
-                putc(curSym, output);
-            }
-            else if (curSym == '\'') {
-                stringStatus = 2;
-                putc(curSym, output);
-            }
+            putc(curSym, output);
+            lastChar = curSym;  //сохранить последний не пробел
 
-
-            //просто код
-            else putc(curSym, output);
+           // смена состояния
+            if (curSym == '"') stringStatus = 1;
+            else if (curSym == '\'') stringStatus = 2;
             break;
 
-        case 1:
+        case 1: //строка
             putc(curSym, output);
-
-            //обработка экранирования
             if (curSym == '\\') {
                 next = getc(input);
                 if (next != EOF) putc(next, output);
             }
-
-            else if (curSym == '"' || curSym == '\n') stringStatus = 0;
+            else if (curSym == '"' || curSym == '\n') stringStatus = 0; //вернуться в код
             break;
 
-        case 2:
+        case 2: //константа
             putc(curSym, output);
-
-            //обработка экранирования
             if (curSym == '\\') {
                 next = getc(input);
                 if (next != EOF) putc(next, output);
             }
-
             else if (curSym == '\'' || curSym == '\n') stringStatus = 0;
             break;
         }
